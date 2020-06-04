e56465966c2e2d444feed0336c90cce07c415c5c
john <john.denotter@computronix.com>
2020-06-04 13:37:18 -0600

    cache variables for promotions

--- UU script.lua
+++ UU script.lua
@@ -1,7 +1,15 @@
 include("FLuaVector.lua")
 include("Sukritact_saveutils.lua"); MY_MOD_NAME = "Neirai-Seneca-Lua";
 
-NTF_UsingHatchet = 
+NTF_AfterRandom = GameInfoTypes.PROMOTION_NTF_SENECA_AFTERRANDOM
+NTF_BeforeRandom = GameInfoTypes.PROMOTION_NTF_SENECA_BEFORERANDOM
+NTF_GunnerMoves = GameInfoTypes.PROMOTION_NTF_SENECA_GUNNER_MOVES
+NTF_HatchetBackup = GameInfoTypes.PROMOTION_NTF_SENECA_HATCHETBACKUP
+NTF_IsGunner = GameInfoTypes.PROMOTION_NTF_SENECA_GUNNER
+NTF_SenecaId = GameInfoTypes.CIVILIZATION_NTF_SENECA
+NTF_RandomDone = GameInfoTypes.PROMOTION_NTF_SENECA_RANDOMDONE
+NTF_UsingHatchet = GameInfoTypes.PROMOTION_NTF_SENECA_USINGHATCHET
+
 
 function FindCivsForStrength(playerId, centerPlot)
   local foundCivs = {}
@@ -37,14 +45,14 @@ end
 
 function GetExtraMoveFromAdjacentEnemy(playerId)
   local playerObj = Players[playerId]
-  if playerObj:GetCivilizationType() == GameInfoTypes.CIVILIZATION_NTF_SENECA then
+  if playerObj:GetCivilizationType() == NTF_SenecaId then
     local playerTeamId = playerObj:GetTeam()
     for unitObj in playerObj:Units() do
-      if unitObj:IsHasPromotionType(GameInfoTypes.PROMOTION_NTF_SENECA_GUNNER) then
+      if unitObj:IsHasPromotionType(NTF_IsGunner) then
         if IsAdjacentToEnemy(playerId, playerTeamId, unitObj) then
-          unitObj:SetHasPromotion(GetInfoTypes.PROMOTION_NTF_SENECA_GUNNER_MOVES, true)
+          unitObj:SetHasPromotion(NTF_GunnerMoves, true)
         else
-          unitObj:SetHasPromotion(GetInfoTypes.PROMOTION_NTF_SENECA_GUNNER_MOVES, false)
+          unitObj:SetHasPromotion(NTF_GunnerMoves, false)
         end
       end
     end
@@ -53,9 +61,9 @@ end
 
 function GetAttackPowerFromNearbyCivs(playerId)
   local playerObj = Players[playerId]
-  if playerObj:GetCivilizationType == GameInfoTypes.CIVILIZATION_NTF_SENECA then
+  if playerObj:GetCivilizationType == NTF_SenecaId then
     for unitObj in playerObj:Units() do
-      if unitObj:IsHasPromotionType(GameInfoTypes.PROMOTION_NTF_SENECA_GUNNER) then
+      if unitObj:IsHasPromotionType(NTF_IsGunner) then
         --reload or remember the base combat strength because it's going to change.
         local cachedStrength = load(unitObj, 'baseCombatStrength')
         if cachedStrength == nil then
@@ -100,7 +108,7 @@ end
 
 function OnTurnGoBackToYourself(playerId)
   local playerObj = Players[playerId]
-  if playerObj:GetCivilizationType == GameInfoTypes.CIVILIZATION_NTF_SENECA then
+  if playerObj:GetCivilizationType == NTF_SenecaId then
     for unitObj in playerObj:Units() do
       unitObj:SetHasPromotion(NTF_UsingHatchet, false)
     end
@@ -112,7 +120,7 @@ function ListenSEUSD(playerId, unitId, newDamage, oldDamage)
   if newDamage > oldDamage then --filter out heals
     local playerObj = Players[playerId]
     local unitObj = PlayerObj:GetUnitById(unitId)
-    if not unitObj:IsHasPromotion(GameInfoTypes.PROMOTION_NTF_SENECA_RANDOMDONE) then
+    if not unitObj:IsHasPromotion(NTF_RandomDone) then
       local unitPlot = unitObj:GetPlot()
       --this unit took damage from somewhere, check around for a unit that dealt damage
       --all the units we care about are melee units
@@ -122,8 +130,7 @@ function ListenSEUSD(playerId, unitId, newDamage, oldDamage)
           if adjacentPlot:GetNumUnits() > 0 then
             for i = 0, adjacentPlot:GetNumUnits() - 1 do
               local adjacentPlotUnitObj = adjacentPlot:GetUnit(i)
-              if adjacentPlotUnitObj:IsHasPromotion(
-                  GameInfoTypes.PROMOTION_NTF_SENECA_HATCHETBACKUP) then
+              if adjacentPlotUnitObj:IsHasPromotion(NTF_HatchetBackup) then
                 local adjacentPlotUnitOwnerId = adjacentPlotUnitObj:GetOwner()
                 local foundCivCount = FindCivsForStrength(
                     adjacentPlotUnitOwnerId, adjacentPlot)
@@ -132,10 +139,8 @@ function ListenSEUSD(playerId, unitId, newDamage, oldDamage)
                 adjacentPlotUnitObj:SetBaseCombatStrength(16)
                 adjacentPlotUnitObj:SetHasPromotion(
                     NTF_UsingHatchet, true) -- range + 1, blitz
-                adjacentPlotUnitObj:SetHasPromotion(
-                    GameInfoTypes.PROMOTION_NTF_SENECA_AFTERRANDOM, false)
-                adjacentPlotUnitObj:SetHasPromotion(
-                    GameInfoTypes.PROMOTION_NTF_SENECA_BEFORERANDOM, true)
+                adjacentPlotUnitObj:SetHasPromotion(NTF_AfterRandom, false)
+                adjacentPlotUnitObj:SetHasPromotion(NTF_BeforeRandom, true)
                 return -- we found the dude, stop doing anything.
               end
             end
@@ -151,9 +156,9 @@ Events.SerialEventUnitSetDamage.Add(ListenSEUSD)
 function ListenPT(playerId) -- all else has failed, we'll do this on the next turn
     local playerObj = Players[playerId]
     for unitObj in playerObj:Units() do
-       if unitObj:IsHasPromotion(GameInfoTypes.PROMOTION_NTF_SENECA_AFTERRANDOM) then
-            unitObj:SetHasPromotion(GameInfoTypes.PROMOTION_NTF_SENECA_AFTERRANDOM, false)
-            unitObj:SetHasPromotion(GameInfoTypes.PROMOTION_NTF_SENECA_BEFORERANDOM, true)
+       if unitObj:IsHasPromotion(NTF_AfterRandom) then
+            unitObj:SetHasPromotion(NTF_AfterRandom, false)
+            unitObj:SetHasPromotion(NTF_BeforeRandom, true)
         end
     end
 end
