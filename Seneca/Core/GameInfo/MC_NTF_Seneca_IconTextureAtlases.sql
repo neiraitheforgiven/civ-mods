09bdf4bb9d7006f17165cac8645219b1686cc8e1
John den Otter <patronjude@gmail.com>
2020-09-14 22:13:12 -0600

    Increase number of icons in atlas

--- Seneca/Core/GameInfo/MC_NTF_Seneca_IconTextureAtlases.sql
+++ Seneca/Core/GameInfo/MC_NTF_Seneca_IconTextureAtlases.sql
@@ -5,14 +5,14 @@
 --------------------------------------------------------------------------------------------------------------------------
 INSERT INTO IconTextureAtlases 
 		(Atlas, 								IconSize, 	Filename, 								IconsPerRow, 	IconsPerColumn)
-VALUES	('MC_NTF_SENECA_ATLAS',					256, 		'MC_NTF_SENECA_256.dds',				2,				2),
-		('MC_NTF_SENECA_ATLAS',					128, 		'MC_NTF_SENECA_128.dds',				2,				2),
-		('MC_NTF_SENECA_ATLAS',					80, 		'MC_NTF_SENECA_80.dds',					2,				2),
-		('MC_NTF_SENECA_ATLAS',					64, 		'MC_NTF_SENECA_64.dds',					2,				2),
-		('MC_NTF_SENECA_ATLAS',					48, 		'MC_NTF_SENECA_48.dds',					2,				2),
-		('MC_NTF_SENECA_ATLAS',					45, 		'MC_NTF_SENECA_45.dds',					2,				2),
-		('MC_NTF_SENECA_ATLAS',					32, 		'MC_NTF_SENECA_32.dds',					2,				2),
-		('MC_NTF_SENECA_ATLAS',					24, 		'MC_NTF_SENECA_24.dds',					2,				2),
+VALUES	('MC_NTF_SENECA_ATLAS',					256, 		'MC_NTF_SENECA_256.dds',				2,				3),
+		('MC_NTF_SENECA_ATLAS',					128, 		'MC_NTF_SENECA_128.dds',				2,				3),
+		('MC_NTF_SENECA_ATLAS',					80, 		'MC_NTF_SENECA_80.dds',					2,				3),
+		('MC_NTF_SENECA_ATLAS',					64, 		'MC_NTF_SENECA_64.dds',					2,				3),
+		('MC_NTF_SENECA_ATLAS',					48, 		'MC_NTF_SENECA_48.dds',					2,				3),
+		('MC_NTF_SENECA_ATLAS',					45, 		'MC_NTF_SENECA_45.dds',					2,				3),
+		('MC_NTF_SENECA_ATLAS',					32, 		'MC_NTF_SENECA_32.dds',					2,				3),
+		('MC_NTF_SENECA_ATLAS',					24, 		'MC_NTF_SENECA_24.dds',					2,				3),
 		('MC_NTF_SENECA_ALPHA_ATLAS',			128, 		'MC_NTF_SENECA_ALPHA_128.dds',			1,				1),
 		('MC_NTF_SENECA_ALPHA_ATLAS',			64, 		'MC_NTF_SENECA_ALPHA_64.dds',			1,				1),
 		('MC_NTF_SENECA_ALPHA_ATLAS',			48, 		'MC_NTF_SENECA_ALPHA_256.dds',			1,				1),
